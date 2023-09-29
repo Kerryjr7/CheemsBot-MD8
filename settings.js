6eb7178fdab974cfe1e5bdf56b34be6f62419621
Kerryjr7 <118448371+Kerryjr7@users.noreply.github.com>
2023-09-29 11:08:55 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['254113545599','6283134496493'] //ur owner number
+global.ownernomer = "254113545599" //ur owner number2
+global.ownername = "BLACK NOIR" //ur owner name
+global.ytname = "YT: DEXTEROUS_MODS_OFC" //ur yt chanel name
+global.socialm = "GitHub: kerryjr7" //ur github or insta name
+global.location = "Kenya, Nairobi, utawala" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "Cheems Bot MD v8"
+global.ownernumber = '6283134496493'
+global.ownername = 'Black noir(kerry jr)'
+global.ownerNumber = ["6283134496493@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@DEXTEROUS_MODS_OFC"
+global.websitex = "https://youtu.be/@DEXTEROUS_MODS_OFC"
+global.wagc = "https://YouTube.com/@DEXTEROUS_MODS_OFC"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.creator = "6283134496493@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
